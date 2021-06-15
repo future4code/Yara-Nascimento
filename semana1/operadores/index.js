0729bd456bb99f9d4d2ac7e3b131e0d1bf0a9885
YaraNascimento <Yara@YaraTacy.local>
2021-06-15 19:11:49 -0300

    Exercícios 3 e 4 de escritas de código

--- semana1/operadores/index.js
+++ semana1/operadores/index.js
@@ -16,3 +16,21 @@ console.log(numeroPar % 2)
 
 // Notei que console.log sempre retorna um valor maior que 0 para divisões 
 // de números ímpares por dois, dado que esse operador resulta o resto da divisão.
+
+
+// Exercício 3
+
+let idadeUsuario2 = prompt("Digite sua idade")
+console.log("Sua idade em meses ficaria ", idadeUsuario2 * 12)
+console.log("Sua idade em dias ficaria ", idadeUsuario2 * 365)
+console.log("Sua idade em horas ficaria ", idadeUsuario2 * 8760)
+
+// Exercício 4
+
+let primeiroNumero = prompt("Digite o primeiro número")
+let segundoNumero = prompt("Digite o segundo número")
+
+console.log("O primeiro número é maior que segundo? ", primeiroNumero > segundoNumero)
+console.log("O primeiro número é igual ao segundo? ", primeiroNumero === segundoNumero)
+console.log("O primeiro número é divisível pelo segundo? ", (primeiroNumero % segundoNumero) == 0)
+console.log("O segundo número é divisível pelo primeiro? ", (segundoNumero % primeiroNumero) == 0)
