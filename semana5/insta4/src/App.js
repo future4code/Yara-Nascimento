4eb84ff4c240189b919a8fd433827c418e70ce69
YaraNascimento <yara.tacy.barbosa.29@gmail.com>
2021-07-15 17:50:52 -0300

    Projeto insta4 feito para teste

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
       <div>
       <MainContainer>
         <Post
-          nomeUsuario={'Yara Tacy'}
+          nomeUsuario={'Tacy Summy'}
           fotoUsuario={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFOsGyDGFYzOgCyxgVZ931wWjOBxVl4zMmiQ&usqp=CAU'}
           fotoPost={'https://a-static.mlcdn.com.br/1500x1500/quadro-decorativo-rick-and-morty-anime-desenho-com-moldura-g02-vital-quadros/vitalquadrosdobrasil/9909808040/f7e02cd5ae7e65695f170951f86198f1.jpg'}
         />
@@ -26,7 +26,7 @@ class App extends React.Component {
       <div>
       <MainContainer>
         <Post
-          nomeUsuario={'Glayton Verissimo'}
+          nomeUsuario={'Rick Verissimo'}
           fotoUsuario={'https://pm1.narvii.com/6335/a61407611ecb2043698c2b58e6371f8aaa3ad891_hq.jpg'}
           fotoPost={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk2ySt6PCoFyxZ9O3ysMUUqxBUGgx_PTQGCQ&usqp=CAU'}
         />
