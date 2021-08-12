150fa770f858a423dbf616fa960e62e010127f98
Letícia Chijo <leticia.chijo@gmail.com>
2021-08-12 16:55:15 -0300

    resolução de conflito

--- semana9/astromatch/src/pages/HomePage/HomePage.js
+++ semana9/astromatch/src/pages/HomePage/HomePage.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HomeContainer, Profile, ProfileImage } from './styled'
+import axios from 'axios'
 
-const HomePage = () => {
-    return (
-        <div>HomePage</div>
-    )
+
+export const HomePage = () => {
+    const [profile, setProfile] = useState({})
+
+    const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/yara-nascimento-lovelace'
+
+
+    useEffect(() => {
+        getProfile()
+    }, [])
+
+    const getProfile = () => {
+        axios.get(url)
+            .then((res) => {
+                setProfile(res.data.profile)
+            })
+            .catch((err) => {
+                console.log(err.response)
+            })
+
+
+
+        return (
+            <HomeContainer>
+                <Profile>
+                    <ProfileImage src={profile.photo}>
+                        <h2>{profile.name}, {profile.age}</h2>
+                        <p>{profile.bio}</p>
+                        <div>
+                            <button>x</button>
+                            <button>s2</button>
+                        </div>
+                    </ProfileImage>
+                </Profile>
+            </HomeContainer>
+        )
+
+    }
 }
-export default HomePage
